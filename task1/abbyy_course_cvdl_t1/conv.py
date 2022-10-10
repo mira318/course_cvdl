099c78b2fef90cfd7817c7317f1b71f8636a3718
mira318 <chuvilyaeva999@yandex.ru>
2022-10-10 23:55:14 +0300

    almost conv((

--- task1/abbyy_course_cvdl_t1/conv.py
+++ task1/abbyy_course_cvdl_t1/conv.py
@@ -22,7 +22,14 @@ class ConvLayer(BaseLayer):
         assert(out_channels > 0)
         assert(kernel_size % 2 == 1)
         super().__init__()
-        raise NotImplementedError()
+        
+        std = 1 / np.sqrt(in_channels)
+        W = np.random.uniform(-std, std, size = [out_channels, in_channels, kernel_size, kernel_size])
+        b = np.random.uniform(-std, std, size = out_channels)
+        W_grad = np.zeros([out_channels, in_channels, kernel_size, kernel_size])
+        b_grad = np.zeros(out_channels)
+        self.parameters = [W, b]
+        self.parameters_grads = [W_grad, b_grad]
 
     @property
     def kernel_size(self):
@@ -37,16 +44,20 @@ class ConvLayer(BaseLayer):
         return self.parameters[0].shape[1]
 
     @staticmethod
-    def _pad_zeros(tensor, one_side_pad, axis=[-1, -2]):
+    def _pad_zeros(self, tensor, one_side_pad, axis=[-1, -2]):
         """
         Добавляет одинаковый паддинг по осям, указанным в axis.
         Метод не проверяется в тестах -- можно релизовать слой без
         использования этого метода.
         """
-        pass
+        offsets = np.zeros(tensor.ndim).astype(int)
+        offsets[axis] = one_side_pad    
+        insertion_mask = ([(offsets[d], offsets[d]) for d in range(tensor.ndim)])
+        padded = np.pad(tensor, insertion_mask, 'constant', constant_values = 0)
+        return padded
 
     @staticmethod
-    def _cross_correlate(input, kernel):
+    def _cross_correlate(self, input, kernel):
         """
         Вычисляет "valid" кросс-корреляцию input[B, C_in, H, W]
         и kernel[C_out, C_in, X, Y].
@@ -55,10 +66,63 @@ class ConvLayer(BaseLayer):
         """
         assert kernel.shape[-1] == kernel.shape[-2]
         assert kernel.shape[-1] % 2 == 1
-        pass
+        self.input = input
+        res = np.zeros((input.shape[0], kernel.shape[0], 
+                        input.shape[2] - self.parameters[0].shape[-1] + 1, 
+                        input.shape[3] - self.parameters[0].shape[-1] + 1))
+        # print('input.shape = ', input.shape)
+        # print('kernel.shape = ', kernel.shape)
+        for b in range(input.shape[0]):
+            for c_o in range(kernel.shape[0]):
+                for c_i in range(input.shape[1]):
+                    for h_place in range(input.shape[2] - self.parameters[0].shape[-1] + 1):
+                        for w_place in range(input.shape[3] - self.parameters[0].shape[-1] + 1):
+                            # print('input[', b, ', ', c_i, ', (', 
+                            #       h_place, '):(', h_place + self.parameters[0].shape[-1], '), (', 
+                            #       w_place, '):(', w_place + self.parameters[0].shape[-1], ')])= ', 
+                            #       input[b, c_i,
+                            #       (h_place):(h_place + self.parameters[0].shape[-1]), 
+                            #       (w_place):(w_place + self.parameters[0].shape[-1])])
+                            
+                            # print('kernel[c_o, c_i, :, :] = ', kernel[c_o, c_i, :, :])
+                                
+                            res[b, c_o, h_place, w_place] += np.sum(
+                                input[b, c_i,
+                                h_place:(h_place + self.parameters[0].shape[-1]), 
+                                w_place:(w_place + self.parameters[0].shape[-1])
+                                ] * kernel[c_o, c_i, :, :])
+                res[b, c_o] += self.parameters[1][c_o]
+                                
+        return res
 
     def forward(self, input: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
+        padding_size = self.parameters[0].shape[-1] // 2
+        input_padded = self._pad_zeros(self, input, padding_size)
+        return self._cross_correlate(self, input_padded, self.parameters[0])
 
     def backward(self, output_grad: np.ndarray)->np.ndarray:
-        raise NotImplementedError()
+        for c_o in range(self.parameters[0].shape[0]):
+            self.parameters_grads[1][c_o] = np.sum(output_grad[:, c_o, :, :])
+            
+        padding_size = self.parameters[0].shape[-1] // 2
+        output_grad_padded = self._pad_zeros(self, output_grad, padding_size)
+        print('output_grad.shape = ', output_grad.shape)
+        grad = np.zeros((output_grad.shape[0], self.parameters[0].shape[1], output_grad.shape[2], output_grad.shape[3]))
+        
+        w_changed = np.zeros((self.parameters[0].shape[-1], self.parameters[0].shape[-1]))
+        
+        for b in range(self.input.shape[0]):
+            for c_o in range(self.parameters[0].shape[0]):
+                for c_i in range(self.input.shape[1]):
+                    for h_place in range(self.input.shape[2] - self.parameters[0].shape[-1] + 1):
+                        for w_place in range(self.input.shape[3] - self.parameters[0].shape[-1] + 1):
+                            for h_kernel in range(self.parameters[0].shape[-1]):
+                                for w_kernel in range(self.parameters[0].shape[-1]):
+                                    self.parameters_grads[0][c_o][c_i][h_kernel][w_kernel] += output_grad_padded[b, c_o, h_place + h_kernel, w_place + w_kernel]
+                                    w_changed[h_kernel, w_kernel] = self.parameters[0][c_o][c_i][self.parameters[0].shape[-1] - 1 - h_kernel][self.parameters[0].shape[-1] - 1 - w_kernel]
+                             
+                             
+                            grad[b, c_i, h_place, w_place] += np.sum(w_changed * output_grad_padded[b, c_o, h_place:(h_place + self.parameters[0].shape[-1]), w_place:(w_place + self.parameters[0].shape[-1])])
+                            
+                                    
+        return grad
