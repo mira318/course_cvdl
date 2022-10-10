664e4ef9a9660b3ea0d9072349e8ad9358ad9fc8
mira318 <chuvilyaeva999@yandex.ru>
2022-10-10 12:50:12 +0300

    done linear

--- task1/abbyy_course_cvdl_t1/linear.py
+++ task1/abbyy_course_cvdl_t1/linear.py
@@ -13,10 +13,39 @@ class LinearLayer(BaseLayer):
     """
     def __init__(self, in_features: int, out_features: int):
         super().__init__()
-        raise NotImplementedError()
+        self.in_features = in_features
+        self.out_features = out_features
+        std = 1 / np.sqrt(in_features)
+        
+        self.W = np.random.uniform(
+            -std, std, size = [self.out_features, self.in_features]
+            )
+        self.b = np.random.uniform(-std, std, size = self.out_features)
+        self.parameters = [self.W, self.b]
+        
+        self.W_grad = np.zeros([self.out_features, self.in_features])
+        self.b_grad = np.zeros(self.out_features)
+        self.parameters_grads = [self.W_grad, self.b_grad]
 
     def forward(self, input: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
+        self.input = input
+        while len(self.input.shape) < 4:
+            self.input = self.input[np.newaxis, ...] 
+        return input @ self.parameters[0].T + self.parameters[1]
 
     def backward(self, output_grad: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
+        self.parameters_grads[0] = np.zeros((self.out_features, self.in_features))
+        self.parameters_grads[1] = np.zeros(self.out_features)
+        output_grad_dims = len(output_grad.shape)
+        while len(output_grad.shape) < 4:
+            output_grad = output_grad[np.newaxis, ...]
+            
+        for b in range(self.input.shape[0]):
+            for c in range(self.input.shape[1]):
+                self.parameters_grads[0] += output_grad[b][c].T @ self.input[b][c]
+                self.parameters_grads[1] += np.sum(output_grad[b][c].T, axis = 1) 
+        res_grad = output_grad @ self.parameters[0]
+        
+        for t in range(4 - output_grad_dims):
+            res_grad = res_grad[0, ...]
+        return res_grad
