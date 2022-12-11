ef30d3b3b96b1d465370b80ae5920ce179950ba7
mira318 <chuviliaeva999@yandex.ru>
2022-12-11 20:32:53 +0000

    done network

--- task_project/tvn1_code/blocks_pkg/blocks.py
+++ task_project/tvn1_code/blocks_pkg/blocks.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from .sublocks import *
 
+
 class TinyBlock1(nn.Module):
     def __init__(self, strides = 0, in_channels = 3, num_frames = 2):
         super().__init__()
@@ -16,8 +17,7 @@ class TinyBlock1(nn.Module):
         self.temporal_avg = MultiAvg(kernel_size = 3, stride = 1, padding = 1)
         
         self.context_gate2 = MultiCG(in_channels = 64, out_channels = 64, 
-                                     kernel_size = 3, stride = 2, padding = 1)
-        
+                                     kernel_size = 3, stride = 2, padding = 1)        
         
     def forward(self, input):
         x, vid_lens = input
@@ -66,7 +66,7 @@ class TinyBlock2(nn.Module):
             
         x = self.se(x)
         return x, vid_lens
-    
+
 class TinyBlock3(nn.Module):
     def __init__(self, in_channels = 128):
         super().__init__()
@@ -75,4 +75,74 @@ class TinyBlock3(nn.Module):
         self.sublock3 = Tiny_3_sublock(in_channels = 128)
         
     def forward(self, input):
-        return self.sublock3(self.sublock2(self.sublock1(input)))
+        x = self.sublock3(self.sublock2(self.sublock1(input)))
+        return x
+    
+    
+class TinyBlock4(nn.Module):
+    def __init__(self, in_channels = 128, out_channels = 512):
+        super().__init__()
+        self.conv1_1 = nn.Conv1d(in_channels = 128, out_channels = 256, 
+                               kernel_size = 3, stride = 1, padding = 1, dilation = 1, groups = 1)
+        self.context_gate1 = MultiCG(in_channels = 256, out_channels = 256, 
+                                     kernel_size = 3, stride = 1, padding = 1)
+        self.se1 = MultiSE(256, 256)
+        
+        self.conv2_1 = nn.Conv1d(in_channels = 256, out_channels = 512, 
+                               kernel_size = 3, stride = 1, padding = 1, dilation = 1, groups = 1)
+        self.context_gate2 = MultiCG(in_channels = 512, out_channels = 512, 
+                                     kernel_size = 3, stride = 1, padding = 1)
+        self.se2 = MultiSE(512, 512)
+        
+        
+    def forward(self, input):
+        x, vid_lens = input
+        x = nn.AdaptiveAvgPool2d((x.shape[2] // 2, x.shape[3] // 2))(x)
+        
+        _, channels, h, w = x.shape
+        new_vid_lens = []
+        out = []
+            
+        for i in range(len(vid_lens)):
+            idx = sum(vid_lens[:i])
+            curr_len = vid_lens[i]
+            
+            part_x = x[idx:(idx + curr_len)]  
+            part_x = part_x.reshape(curr_len, channels, h * w)
+            y = torch.transpose(part_x, 2, 0)
+            
+            y = self.conv1_1(y)
+            y = torch.transpose(y, 2, 0)
+            y = y.reshape(y.shape[0], y.shape[1], h, w)
+                
+            new_vid_lens.append(y.shape[0])
+            out.append(y)
+                
+        x = torch.cat(out, dim = 0)
+        vid_lens = new_vid_lens
+        x = self.se1(self.context_gate1(x))
+        
+        x = nn.AdaptiveAvgPool2d((x.shape[2] // 2, x.shape[3] // 2))(x)
+        _, channels, h, w = x.shape
+        new_vid_lens = []
+        out = []
+            
+        for i in range(len(vid_lens)):
+            idx = sum(vid_lens[:i])
+            curr_len = vid_lens[i]
+            part_x = x[idx:(idx + curr_len)]
+                
+            part_x = part_x.reshape(curr_len, channels, h * w)
+            y = torch.transpose(part_x, 2, 0)
+            y = self.conv2_1(y)
+            y = torch.transpose(y, 2, 0)
+            y = y.reshape(y.shape[0], y.shape[1], h, w)
+                
+            new_vid_lens.append(y.shape[0])
+            out.append(y)
+                
+        x = torch.cat(out, dim = 0)
+        vid_lens = new_vid_lens
+        x = self.se2(self.context_gate2(x))
+        
+        return x, vid_lens
--- task_project/tvn1_code/tvn1.py
+++ task_project/tvn1_code/tvn1.py
@@ -3,15 +3,38 @@ from torch import nn
 from blocks_pkg.blocks import *
 
 class TVN1(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes):
         super().__init__()
+        self.num_classes = num_classes
         self.body = nn.Sequential(
             TinyBlock1(),
             TinyBlock2(),
-            TinyBlock3()
+            TinyBlock3(),
+            TinyBlock4()
         )
+        self.head_linear1 = nn.Linear(512, 512)
+        self.head_lrelu = nn.LeakyReLU(0.2)
+        self.head_linear2 = nn.Linear(512, num_classes)
         
     def forward(self, input):
         x, vid_lens = input
         x, vid_lens = self.body((x, vid_lens))
+        
+        _, channels, h, w = x.shape
+        out = []
+            
+        for i in range(len(vid_lens)):
+            idx = sum(vid_lens[:i])
+            curr_len = vid_lens[i]
+            part_x = x[idx:(idx + curr_len)]
+                
+            y = torch.mean(part_x, dim = [0, 2, 3], keepdim = True)
+            y = y.squeeze(-1).squeeze(-1)    
+            out.append(y)
+                
+        x = torch.cat(out, dim = 0)
+        print('before head x.shape = ', x.shape)
+        x = self.head_linear1(x)
+        x = self.head_lrelu(x)
+        x = self.head_linear2(x)
         return x
