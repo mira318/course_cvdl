f2b64633b134678c013912ab8f2bc1e0d44e7202
mira318 <chuviliaeva999@yandex.ru>
2022-12-11 02:32:20 +0000

    done the first block

--- task_project/tvn1_code/blocks_pkg/sublocks.py
+++ task_project/tvn1_code/blocks_pkg/sublocks.py
@@ -1,41 +1,47 @@
 import torch
+import math
 from torch import nn
 
 class MultiMaxPool(nn.Module):
     def __init__(self, kernel_size, stride = 1, padding = 0):
         super().__init__()
-        self.kerne_size = kernel_size
+        self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
         
     def forward_single(self, x):
-        batch_frames, c, h, w = x.shape()
+        batch_frames, c, h, w = x.shape
         x = torch.transpose(x.reshape(batch_frames, c, h * w), 2, 0)
-        y = nn.MaxPool1d(x, kernel_size = self.kernel_size, stride = self.stride, 
-                         padding = self.padding)
+        y = nn.MaxPool1d(kernel_size = self.kernel_size, stride = self.stride, padding = self.padding)(x)
         y = torch.transpose(y, 2, 0)
-        y = y.reshape(y.shape[0], y.shape[1], 2, 2)
+        y = y.reshape(y.shape[0], y.shape[1], h, w)
         return y
     
-    def forward(self, x, batch_frames):
-        new_batch_frames = []
+    def forward(self, x, vid_lens):
+        new_vid_lens = []
         out = []
-        for i in range(len(batch_frames)):
-            idx = torch.sum(batch_frames[:i])
-            curr_len = batch_frames[i]
+        for i in range(len(vid_lens)):
+            idx = sum(vid_lens[:i])
+            curr_len = vid_lens[i]
             
             part_x = x[idx:(idx + curr_len)]
             y = self.forward_single(part_x)
             
-            new_btches_frames.append(y.shape[0])
+            new_vid_lens.append(y.shape[0])
             out.append(y)
             
-        return torch.cat(out, dim = 0), new_vid_len
+        return torch.cat(out, dim = 0), new_vid_lens
 
 class MultiCG(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride = 1 , padding = 0,
                  dilation = 1, groups = 1, bias = True):
         super().__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        
+        self.kernel_size = kernel_size
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.padding = padding
         
         if kernel_size == 1:
             self.simple = True
@@ -68,18 +74,19 @@ class MultiCG(nn.Conv2d):
             self.unfold = nn.Unfold(kernel_size, dilation, padding, stride)
             self.sigmoid = nn.Sigmoid()            
     
-    def forward():
+    def forward(self, x):
         if self.simple:
-            return nn.Conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+            return nn.Conv2d(self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)(x)
         else:
             batch, channels, h, w = x.shape
-            out = self.linear1(self.avg_pool(x.reshape(batch, channels, h * w)))
+            out = self.avg_pool(x)
+            out = self.linear1(out.reshape(batch, channels, -1))
             features = out
             out = self.linear3(self.relu(self.bn1(out)))
             
             if self.reduced_features > 3:
                 features = self.bn2(features)
-                features = features.reshape(batch, channels // self.reduced_features, reduced_features, -1)
+                features = features.reshape(batch, channels // self.reduced_features, self.reduced_features, -1)
                 features = features.transpose(2, 3)
                 features = self.linear2(self.relu(features))
                 features = features.transpose(2, 3).contiguous() 
@@ -98,10 +105,10 @@ class MultiCG(nn.Conv2d):
             
             x_unfolded = self.unfold(x)
             batch, c_w, last = x_unfolded.shape
-            out = out * weight.unsqueeae(0)
+            out = out * self.weight.unsqueeze(0)
             out = out.reshape(batch, self.out_channels, -1)
             out = torch.matmul(out, x_unfolded)
-            out = out.reshape(batch, self.out_channels, int(torch.sqrt(last)), int(torch.sqrt(last)))
+            out = out.reshape(batch, self.out_channels, int(math.sqrt(last)), int(math.sqrt(last)))
             return out
         
 class MultiAvg(nn.Module):
@@ -112,20 +119,19 @@ class MultiAvg(nn.Module):
         self.padding = padding
     
     def forward(self, x, vid_lens):
+        lens_sum, channels, h, w = x.shape
         new_vid_lens = []
         out = []
         for i in range(len(vid_lens)):
-            idx = torch.sum(vid_lens[:i])
+            idx = sum(vid_lens[:i])
             curr_len = vid_lens[i]
             part_x = x[idx:(idx + curr_len)]
             
             part_x = part_x.reshape(curr_len, channels, h * w)
-            part_x = part_x.transpose(part_x, 2, 0)
-            y = nn.AvgPool1d(kernel_size = self.kernel_size, 
-                             stride = self.stride, 
-                             padding = self.padding
-                            )(part_x)
-            y = y.transpose(y, 2, 0)
+            part_x = torch.transpose(part_x, 2, 0)
+            y = nn.AvgPool1d(kernel_size = self.kernel_size, stride = self.stride, 
+                             padding = self.padding)(part_x)
+            y = torch.transpose(y, 2, 0)
             y = y.reshape(y.shape[0], y.shape[1], h, w)
             
             new_vid_lens.append(y.shape[0])
--- task_project/tvn1_code/tvn1.py
+++ task_project/tvn1_code/tvn1.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from blocks import *
+from blocks_pkg.blocks import *
 
 class TVN1(nn.Module):
     def __init__(self):
@@ -8,11 +8,6 @@ class TVN1(nn.Module):
         self.body = nn.Sequential(TinyBlock1())
         
     def forward(self, input):
-        # batches_frames, channels, leng, h, w = input.shape
-        # input = input.permute(0, 2, 1, 3, 4)
-        # input = input.reshape(batches_frames * leng, channels, h, w, [l for _ in range(batches_frames)])
-        
-        print('self.body[0] = ', self.body[0])
         x, vid_lens = input
         x, vid_lens = self.body((x, vid_lens))
         return x
